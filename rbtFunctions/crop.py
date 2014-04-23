b0f3ef5a10b7fca3874e8bb4bf16ebb3e57892eb
lukemetz <luke.s.metz@gmail.com>
2014-04-23 14:11:47 -0400

    changed around crop to use calculated hz instead of ukf hz. Improved plotting

--- rbtFunctions/crop.py
+++ rbtFunctions/crop.py
@@ -2,57 +2,62 @@ import os
 import numpy as np
 from util import files,geom,num,util
 from scipy.signal import gaussian
+import matplotlib
+from matplotlib import pyplot as plt
 
 def crop(self,dbg=False,**kwds):
   """
   Estimates when the robot starts and stops running. Tries to remove start up noise as well as ending irregularities.
 
   Prerequisites: {ukf| load}
-  Writes: 
+  Writes:
         self.start_trial
         self.stop_trial
         (self.is_valid)
   """
   t = self.t; X = self.X; N,_ = X.shape; j = self.j; u = self.u
-  speed = np.sqrt(np.diff(X[...,j['x']])**2 + np.diff(X[...,j['y']])**2)*self.hz
-  window = 80
+  working_hz = np.mean((1./np.diff(t)))
+  speed = np.sqrt(np.diff(X[...,j['x']])**2 + np.diff(X[...,j['y']])**2)*working_hz
+  #window = 80
+  window = 300
   std = 40
   gaus = gaussian(window, std)
   #normalize
-  gaus /= np.mean(gaus) 
+  gaus /= np.mean(gaus)
   gaus /= window
 
   speed_conv = np.convolve(speed, gaus, mode="same")
   #sketch root finding... and cropping....
   find_at_speed = 100
-  split_seconds = 2
-  margin_seconds = (.4, .4)
+  margin_seconds = (.4, .1)
 
   def bad_root_find():
-    first_bit = np.abs(speed_conv[0:self.hz*split_seconds] - find_at_speed)
+    split_seconds = 2
+    first_bit = np.abs(speed_conv[0:working_hz*split_seconds] - find_at_speed)
     first_idx = np.argmin(first_bit)
-    second_bit = np.abs(speed_conv[self.hz*split_seconds:] - find_at_speed)
-    second_idx = np.argmin(second_bit) + split_seconds * self.hz
+    second_bit = np.abs(speed_conv[working_hz*split_seconds:] - find_at_speed)
+    second_idx = np.argmin(second_bit) + split_seconds * working_hz
     return first_idx, second_idx
-  
-  def root_find():
 
+  def root_find():
+    split_seconds = 2
     first_idx = 0
     second_idx = 0
-    for idx in range(self.hz*split_seconds):
+    for idx in range(int(working_hz*split_seconds)):
+      first_idx = idx
       if speed_conv[idx] >= find_at_speed:
-        first_idx = idx
         break
-    for idx in range(self.hz*split_seconds, len(speed_conv)):
+    for idx in range(int(working_hz*split_seconds), len(speed_conv)):
+      second_idx = idx
       if speed_conv[idx] <= find_at_speed:
-        second_idx = idx
         break
     return first_idx, second_idx
 
   first_idx, second_idx = root_find()
 
-  first_idx += margin_seconds[0] * self.hz
-  second_idx -= margin_seconds[1] * self.hz
+
+  first_idx += margin_seconds[0] * working_hz
+  second_idx -= margin_seconds[1] * working_hz
 
   if (second_idx - first_idx <= 250):
     print "ERROR on file" + self.fi
@@ -67,23 +72,37 @@ def crop(self,dbg=False,**kwds):
     fig = plt.figure(1)
     ax = fig.add_subplot(111)
     ax.plot(t[1:N], speed)
-    ax.plot([first_idx / float(self.hz), first_idx / float(self.hz)], [0, max_speed], linewidth=3.0)
-    ax.plot([second_idx / float(self.hz), second_idx / float(self.hz)], [0, max_speed], linewidth=3.0)
+    ax.plot([t[first_idx], t[first_idx]], [0, max_speed], linewidth=3.0)
+    ax.plot([t[second_idx], t[second_idx]], [0, max_speed], linewidth=3.0)
+    ax.plot([t[1], t[N-1]], [find_at_speed, find_at_speed], linewidth=2.0)
     ax.set_ylim(-100.,2100.)
     ax.set_xlabel("time (s)")
     ax.set_ylabel("Speed (mm/s)")
     ax.set_title("Cropping bars on time unsmoothed")
-    
+
     fig = plt.figure(2)
     ax = fig.add_subplot(111)
-    ax.plot(t[1:N], speed_conv)
-    ax.plot([first_idx / float(self.hz), first_idx / float(self.hz)], [0, max_speed], linewidth=3.0)
-    ax.plot([second_idx / float(self.hz), second_idx / float(self.hz)], [0, max_speed], linewidth=3.0)
+    ax.plot(t[0:N-1], speed_conv)
+    ax.plot([t[first_idx], t[first_idx]], [0, max_speed], linewidth=3.0)
+    ax.plot([t[second_idx], t[second_idx]], [0, max_speed], linewidth=3.0)
+    ax.plot([t[1], t[N-1]], [find_at_speed, find_at_speed], linewidth=2.0)
     ax.set_ylim(-100.,2100.)
     ax.set_xlabel("time (s)")
     ax.set_ylabel("Speed (mm/s)")
     ax.set_title("Cropping bars on time smoothed")
+
+    fig = plt.figure(3)
+    ax = fig.add_subplot(111)
+    x = X[...,j['x']]
+    y = X[...,j['y']]
+    ax.plot(x,y)
+    ax.plot([x[first_idx]], [y[first_idx]], 'ro')
+    ax.plot([x[second_idx]], [y[second_idx]], 'bo')
+
     plt.show()
+
+
+
 
   self.start_trial = first_idx
   self.stop_trial = second_idx
