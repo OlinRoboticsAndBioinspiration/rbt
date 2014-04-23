9334aa2f6d2ee4f0716ca1ae6855e36d85378f8e
lukemetz <luke.s.metz@gmail.com>
2014-04-23 14:09:39 -0400

    Make cfg file parser more robust.

--- rbtFunctions/cfg_metrics.py
+++ rbtFunctions/cfg_metrics.py
@@ -11,6 +11,7 @@ def cfg_metrics(self, **kwds):
   di,fi = os.path.split(self.fi)
   run_config = eval(open(os.path.join(di,fi+"_cfg.py")).read())
   file_name = run_config["cfg_file"]
-  cfgs = file_name.split(".")[0].split("x")
+  file_name = file_name.split("\\")[-1]
+  cfgs = file_name.split(".")[-2].split("x")
   self.metrics_data["motor_cfg_0"] = float(cfgs[0])
   self.metrics_data["motor_cfg_1"] = float(cfgs[1])
