bd6abe45d77736d08b0e1e2839d506f544b3d811
Luke Metz <luke.metz@students.olin.edu>
2015-04-07 12:06:27 -0400

    support for phase offset in metric parsing.

--- rbtFunctions/cfg_metrics.py
+++ rbtFunctions/cfg_metrics.py
@@ -5,13 +5,26 @@ def cfg_metrics(self, **kwds):
   """
   Writes metrics about the robot trial configuration.
   For now, just writing motor configuration thats set in run_config
-  Prerequisites: 
+  Filenames take the form of 10x10 or 10x10@340
+  Prerequisites:
   Effects: Sets motor_cfg_0 metric, and motor_cfg_1 metric
   """
   di,fi = os.path.split(self.fi)
   run_config = eval(open(os.path.join(di,fi+"_cfg.py")).read())
   file_name = run_config["cfg_file"]
   file_name = file_name.split("\\")[-1]
-  cfgs = file_name.split(".")[-2].split("x")
+  print file_name
+  possible_offset = file_name.split("@")
+  offset = None
+  if len(possible_offset) > 1:
+    just_x = possible_offset[0]
+    offset = possible_offset[1].split(".")[-2]
+  else:
+    just_x = possible_offset[0].split(".")[-2]
+
+  cfgs = just_x.split("x")
   self.metrics_data["motor_cfg_0"] = float(cfgs[0])
   self.metrics_data["motor_cfg_1"] = float(cfgs[1])
+  if offset is not None:
+    self.metrics_data["motor_phase_offset"] = float(offset)
+  print self.metrics_data
